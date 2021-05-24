55d2467b24b3e263c02770aede28651bea60544e
pikokr <parangee9706@gmail.com>
2021-05-24 12:11:27 +0900

    feat: findByUsername

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common'
+import { UserRepository } from './user.repository'
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private userRepository: UserRepository) {}
+
+  async findByUsername(username: string) {
+    return await this.userRepository.findOne({
+      where: {
+        username,
+      },
+    })
+  }
+}
