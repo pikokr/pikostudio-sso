3769477cd5d2851aff7bc9a776692dc61c55c043
pikokr <parangee9706@gmail.com>
2021-05-24 11:31:45 +0900

    feat: register user repository

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common'
 import { UsersService } from './users.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { UserRepository } from './user.repository'
 
 @Module({
   providers: [UsersService],
+  imports: [TypeOrmModule.forFeature([UserRepository])],
 })
 export class UsersModule {}
