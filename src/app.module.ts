c5e46f076d484df1478bb2867c9746eb1eeee3d3
pikokr <parangee9706@gmail.com>
2021-05-24 10:29:05 +0900

    fix: env not loading

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import { TypeOrmModule } from '@nestjs/typeorm'
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABSE,
-      entities: [],
+      autoLoadEntities: true,
       synchronize: true,
     }),
   ],
