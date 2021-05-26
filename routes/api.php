2ee26f73fa170f8f6a0eb23c6b94ee0632bcfb9e
pikokr <parangee9706@gmail.com>
2021-05-27 08:48:40 +0900

    feat: api user route

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    $user = $request->user();
+    if (!$user->tokenCan('discord')) {
+        $user = $user->makeHidden('discord_id');
+    }
+    return $user;
 });
